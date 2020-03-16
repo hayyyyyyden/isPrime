0f76dbd59baa20685c85c7f97e24648d68f5ecac
yixiong <hide.fyx@gmail.com>
2020-03-16 14:34:53 +1100

    v4: check only prime between 7 to 30.

--- isPrime/isPrime/main.swift
+++ isPrime/isPrime/main.swift
@@ -20,13 +20,20 @@ let timeNow = DispatchTime.now()
 
 let max: Double = Double(num).squareRoot()
 
-// check if num can be divided by 2 or 3
-if num % 2 == 0 || num % 3 == 0{
+// check if num can be divided by 2 or 3 or 5
+if num % 2 == 0 || num % 3 == 0 || num % 5 == 0{
     isPrime = true
 } else {
-//    if not, then check only 6k+1 and 6k+5, because 6k+2,6k+3,6k+4 can all be divided by 2 or 3, thus filter out to speed up.
-    for i in stride(from: 5, through: Int(max), by: 6) {
-        if num % i == 0 || num % (i + 2) == 0 {
+//    if not, then check only the prime between 7 to 30, then plus 30 by each iteration.
+    for i in stride(from: 7, through: Int(max), by: 30) {
+        if  num % i == 0
+            || num % (i + 4) == 0
+            || num % (i + 6) == 0
+            || num % (i + 10) == 0
+            || num % (i + 12) == 0
+            || num % (i + 16) == 0
+            || num % (i + 22) == 0
+            || num % (i + 24) == 0{
             isPrime = false
             break
         }
