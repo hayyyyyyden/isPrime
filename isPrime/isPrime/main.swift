d927181ddc2482851fd0ccc9c1927cc1d5225e6a
yixiong <hide.fyx@gmail.com>
2020-03-16 14:03:23 +1100

    v2: check only the odd divider.

--- isPrime/isPrime/main.swift
+++ isPrime/isPrime/main.swift
@@ -10,12 +10,20 @@ let num: Int = input!
 if num < 1 {
     print("Number was not positive")
     exit(2)
+} else if num < 4 {
+    print("\(num) is prime")
+    exit(0)
 }
 
 var isPrime: Bool = true
 let max: Double = Double(num).squareRoot()
-if num > 3 {
-    for i in 2...Int(max) {
+print(max)
+// check if num is odd or even
+if num % 2 == 0 {
+    isPrime = true
+} else {
+    for i in stride(from: 3, through: Int(max), by: 2) {
+        print(i)
         if num % i == 0 {
             isPrime = false
             break
